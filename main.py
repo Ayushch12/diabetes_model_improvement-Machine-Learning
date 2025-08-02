591db722f353bd68c2eeb6762bb9458a71e18e91
hamza <hamza.zaman@walee.pk>
2025-08-02 22:49:29 +0200

    setup project and initail commit of data loading

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+from src.load_data import load_data
+from src.preprocess import preprocess_data
+from src.train import train_model
+from src.evaluate import evaluate_model
+from utils.plot_confusion_matrix import plot_conf_matrix
 
+from sklearn.model_selection import train_test_split
+import joblib
+import os
+
+# Step 1: Load Data
+df = load_data("data/diabetes.csv")
+print(df.columns)
